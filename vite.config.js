3f289b7a8e6a1f41594d835b47e6998098f065c7
浩然 <netcasemail@163.com>
2021-12-16 20:10:50 +0800

    change publicDir

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ const { resolve } = require("path");
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/website-iframe/",
+  publicDir: "/website-iframe/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
